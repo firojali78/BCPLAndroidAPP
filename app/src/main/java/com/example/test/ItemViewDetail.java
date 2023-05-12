00fc05033ee9dfbabd9d7b95f68b92223b567320
ayushmawasthi <ayushmawasthi@gmail.com>
2023-05-12 15:56:11 +0530

    added itemviewdetail api

--- app/src/main/java/com/example/test/ItemViewDetail.java
+++ app/src/main/java/com/example/test/ItemViewDetail.java
@@ -12,9 +12,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
@@ -23,6 +32,8 @@ import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ItemViewDetail extends AppCompatActivity {
     EditText e1,e2,e3,e4,e5,e6;
@@ -33,6 +44,18 @@ public class ItemViewDetail extends AppCompatActivity {
     private float x1,x2;
     static final int MIN_DISTANCE = 150;
     int max_size;
+
+    String session_id = "C1";
+    String session_username = "Akash";
+    String store="";
+    String customer_id="";
+    String category_id ="";
+    String json="";
+
+
+    public interface VolleyCallback{
+        void onSuccess(String result);
+    }
 
 
     @Override
@@ -45,29 +68,40 @@ public class ItemViewDetail extends AppCompatActivity {
         }
 
         init_layout();
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            session_username = extras.getString("session_username");
+            session_id = extras.getString("session_id");
+            store = extras.getString("store_name");
+            customer_id = extras.getString("customer_id");
+            category_id = extras.getString("catagory_id");
 
-        sharedPreferences = getSharedPreferences("Pref", MODE_PRIVATE);
-        SharedPreferences.Editor edit = sharedPreferences.edit();
-        edit.putInt("json_val", 0);
-        edit.commit();
+            Toast.makeText(this, session_id, Toast.LENGTH_SHORT).show();
+            //The key argument here must match that used in the other activity
+        }
+
 
         SharedPreferences sh = getSharedPreferences("Pref", Context.MODE_PRIVATE);
         int index = sh.getInt("json_val",0);
 
-        String name, url,pid;
-        JSONObject jsonObject = loadJson(index);
-        try {
-            name = jsonObject.getString("name");
-            url = jsonObject.getString("imgUrl");
-            pid = jsonObject.getString("pid");
-            edit.putString("pid", pid);
-            edit.commit();
-            setView(name,url);
-            enableEditText(jsonObject);
 
-        } catch (JSONException e) {
-            e.printStackTrace();
+        try {
+            say(category_id,new VolleyCallback(){
+                @Override
+                public void onSuccess(String result){
+                    System.out.println("After callback "+result);
+                    json = result;
+                    loadJson(index);
+                }
+            });
+        } catch (AuthFailureError e) {
+            throw new RuntimeException(e);
         }
+        System.out.println("Starting activity with json value "+json);
+
+
+
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -149,27 +183,74 @@ public class ItemViewDetail extends AppCompatActivity {
 
     private JSONObject loadJson(int i)
     {
+        JSONObject jsonObject =null;
         try {
-            InputStream inputStream = getAssets().open("data.json");
-            int size = inputStream.available();
-            byte [] buffer = new byte[size];
-            inputStream.read(buffer);
-            inputStream.close();
-            String json;
-            json = new String(buffer, StandardCharsets.UTF_8);
+            System.out.println("calling load "+json);
             JSONArray jsonArray = new JSONArray(json);
             int max = jsonArray.length();
             max_size = max;
-            JSONObject jsonObject = jsonArray.getJSONObject(i);
-            return  jsonObject;
+            sharedPreferences = getSharedPreferences("Pref", MODE_PRIVATE);
+            SharedPreferences.Editor edit = sharedPreferences.edit();
+            edit.putInt("json_val", 0);
+            edit.commit();
+            String name, url,pid;
+            jsonObject = jsonArray.getJSONObject(i);
+            try {
+                name = jsonObject.getString("Name");
+                url = jsonObject.getString("Image_URL");
+                pid = jsonObject.getString("Item_No");
+                edit.putString("pid", pid);
+                edit.commit();
+                setView(name,url);
+                enableEditText(jsonObject);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         }
         catch (Exception e)
         {
             Log.d("TAG", "loadJson err "+e);
         }
-        return null;
+        return jsonObject;
+
     }
 
+    private String say(String category_code, final VolleyCallback callback) throws AuthFailureError {
+
+        final String[] res = new String[1];
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://49.249.232.210:6262/webitemmasterapp?catagory_code="+category_code;
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        System.out.println(response+" Response from API");
+                        callback.onSuccess(response);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error.toString()+" Please check your internet");
+
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        if(stringRequest.hasHadResponseDelivered()) {
+            return res[0];
+        }
+        else {
+            return null;
+        }
+    }
 
     private void init_layout() {
         e1  = findViewById(R.id.et_s);
@@ -185,7 +266,7 @@ public class ItemViewDetail extends AppCompatActivity {
 
     private void enableEditText(JSONObject jsonObject) throws JSONException {
         String size;
-        size = jsonObject.getString("size");
+        size = jsonObject.getString("Item_Size");
         String size_arr[] = size.split(",");
         for(int i =0;i<size_arr.length;i++)
         {
@@ -220,6 +301,7 @@ public class ItemViewDetail extends AppCompatActivity {
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
+
         switch(event.getAction())
         {
             case MotionEvent.ACTION_DOWN:
@@ -235,6 +317,7 @@ public class ItemViewDetail extends AppCompatActivity {
                     if (x2 > x1)
                     {
                         SharedPreferences sh = getSharedPreferences("Pref", Context.MODE_PRIVATE);
+                        System.out.println("Current index "+sh.getInt("json_val",0));
                         int index = sh.getInt("json_val",0);
                         String name, url, pid="1";
                         if(index == 0)
@@ -245,9 +328,9 @@ public class ItemViewDetail extends AppCompatActivity {
                             index = index - 1;
                             JSONObject jsonObject = loadJson(index);
                             try {
-                                name = jsonObject.getString("name");
-                                url = jsonObject.getString("imgUrl");
-                                pid = jsonObject.getString("pid");
+                                name = jsonObject.getString("Name");
+                                url = jsonObject.getString("Image_URL");
+                                pid = jsonObject.getString("Item_No");
                                 setView(name, url);
                                 enableEditText(jsonObject);
 
@@ -267,8 +350,8 @@ public class ItemViewDetail extends AppCompatActivity {
                     {
 
                         SharedPreferences sh = getSharedPreferences("Pref", Context.MODE_PRIVATE);
+                        System.out.println("Current index "+sh.getInt("json_val",0));
                         int index = sh.getInt("json_val",0);
-                        System.out.println(max_size + " " + index);
                         if(index == max_size-1)
                         {
                             Toast.makeText(this, "This is Last item", Toast.LENGTH_SHORT).show();
@@ -278,13 +361,14 @@ public class ItemViewDetail extends AppCompatActivity {
                             String name, url, pid = "1";
                             JSONObject jsonObject = loadJson(index);
                             try {
-                                name = jsonObject.getString("name");
-                                url = jsonObject.getString("imgUrl");
-                                pid = jsonObject.getString("pid");
+                                name = jsonObject.getString("Name");
+                                url = jsonObject.getString("Image_URL");
+                                pid = jsonObject.getString("Item_No");
                                 setView(name, url);
                                 enableEditText(jsonObject);
 
                             } catch (JSONException e) {
+                                System.out.println("Error in "+ json +"with index = "+index);
                                 e.printStackTrace();
                             }
                             SharedPreferences.Editor edit = sharedPreferences.edit();
@@ -305,4 +389,6 @@ public class ItemViewDetail extends AppCompatActivity {
         }
         return super.onTouchEvent(event);
     }
+
+
 }
